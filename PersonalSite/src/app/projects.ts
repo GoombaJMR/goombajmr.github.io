ffe435a1ae5a43f8e16f4cc916438378ff3ce9b4
Julian M. Rice <rice.m.julian@gmail.com>
2021-01-03 15:58:44 +0800

    Minor project additions (constants)

---
+++ PersonalSite/src/app/projects.ts
@@ -0,0 +1,17 @@
+export const projects = [
+    {
+        "projectName":"Shujinkou",
+        "projectJp":"主人公",
+        "timeFrame":"October 2018-PRESENT"
+    },
+    {
+        "projectName":"Sinistrous",
+        "projectJp":"シニストレス",
+        "timeFrame":"August 2020-PRESENT"
+    },
+    {
+        "projectName":"Pearl",
+        "projectJp":"パール",
+        "timeFrame":"October 2019-January 2020"
+    }
+]
